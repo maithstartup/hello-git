c1add8017e50294e5d6a63b256c9a37cf20edd07
maithreyan <maithstartup@gmail.com>
2021-01-06 16:43:50 +0530

    second_commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@
 	public class Hello {
 	   public static void main(String[] args) {
 		         System.out.println("Hello, world from GIT! maith");
+			 System.out.println("first commit");													 
 			    }
 			}
